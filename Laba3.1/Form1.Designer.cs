5e236a6e21ea44d114a67eafc40317f3d5a295f1
graphFatigue <94828251+graphFatigue@users.noreply.github.com>
2022-06-08 18:07:02 +0300

    Update Form1.Designer.cs

--- Laba3.1/Form1.Designer.cs
+++ Laba3.1/Form1.Designer.cs
@@ -73,7 +73,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1339, 681);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtFigure);
